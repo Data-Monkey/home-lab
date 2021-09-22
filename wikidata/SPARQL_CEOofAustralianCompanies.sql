1419f922d0df8ca5e970bbd40062aaf4cb4b53b6
Roland Beck <7224371+Data-Monkey@users.noreply.github.com>
2021-09-22 14:28:36 +1000

    Update SPARQL_CEOofAustralianCompanies.sql

--- wikidata/SPARQL_CEOofAustralianCompanies.sql
+++ wikidata/SPARQL_CEOofAustralianCompanies.sql
@@ -1,16 +1,15 @@
 select distinct ?pers ?persLabel ?genderLabel ?orgLabel 
 where {
-    ?pers wdt:P31 wd:Q5.  # Any instance of a human.
-#    ?pers wdt:P27 wd:Q408.  # Australian
+    VALUES ?countries {wd:Q408 wd:Q664}
+    ?pers wdt:P31 wd:Q5.                  # Any instance of a human.
+#    ?pers wdt:P27 wd:Q408.                # Australian
     optional {?pers wdt:P21 ?gender.}
-    ?pers p:P39 ?positions.
-    ?positions ps:P39 wd:Q484876. # Position is CEO
-    ?positions pq:P642 ?org.
-    ?org wdt:P17 wd:Q408.   # Org in Australia
-               
+    ?pers p:P39|p:P106 ?positions.        # Position held / occupation
+    ?positions ps:P39|ps:P106 wd:Q484876. # Position is CEO
+    ?positions pq:P642 ?org.              # CEO of ?org
+    ?org wdt:P17 ?countries.              # Org in Countries
    
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 }
 order by ?orgLabel
 limit 200
-
